e098187f0ef3c140df826478bb8a8190e7c2ec27
Zoheb <rahiman-zoheb@users.noreply.github.com>
2026-02-15 14:49:27 -0800

    Initial Commit

---
+++ android/Mealzy/src/main/java/com/example/mealzy/Main.java
@@ -0,0 +1,11 @@
+package com.example.mealzy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
